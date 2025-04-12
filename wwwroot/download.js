b1a96a9d9963e70443ceb5436085dba711bd6a85
GitMax94 <maximilian.kaestner@gmail.com>
2025-04-13 00:46:06 +0200

    Add project files.

---
+++ wwwroot/download.js
@@ -0,0 +1,6 @@
+﻿window.downloadFileFromBase64 = (filename, base64) => {
+    const link = document.createElement('a');
+    link.download = filename;
+    link.href = 'data:application/zip;base64,' + base64;
+    link.click();
+};
